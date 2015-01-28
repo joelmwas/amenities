4a70092157e7b5eb58d8bacd26d46da75faa607e
Brighton Mwasaru <bmwasaru@gmail.com>
2015-01-28 21:05:23 +0300

    change service_model name

--- application/models/services_model.php
+++ application/models/services_model.php
@@ -8,28 +8,28 @@
 class Services_model extends CI_Model {
     public $table = 'services';
 
-    public function get_all()
+    public function get_all_services()
     {
         return $this->db->get($this->table)->result();
     }
 
-    public function get($id)
+    public function get_service($id)
     {
         return $this->db->where('id', $id)->get($this->table)->row();
     }
 
-    public function add($data)
+    public function add_service($data)
     {
         $this->db->insert($this->table, $data);
         return $this->db->insert_id();
     }
 
-    public function update($id, $data)
+    public function update_service($id, $data)
     {
         return $this->db->where('id', $id)->update($this->table, $data);
     }
 
-    public function delete($id)
+    public function delete_service($id)
     {
         return $this->db->where('id', $id)->delete($this->table);
         return $this->db->affected_rows();
